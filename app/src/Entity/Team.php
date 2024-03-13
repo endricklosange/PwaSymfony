a1d98b34e3dfecb49812fa27d73eb274fc6f4e89
endricklosange <endricklosange@gmail.com>
2024-03-13 10:31:34 +0100

    feat(create entity team and crud)

---
+++ app/src/Entity/Team.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeamRepository::class)]
+class Team
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $score = null;
+
+    #[ORM\Column]
+    private ?int $my_rank = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): static
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getMyRank(): ?int
+    {
+        return $this->my_rank;
+    }
+
+    public function setMyRank(int $my_rank): static
+    {
+        $this->my_rank = $my_rank;
+
+        return $this;
+    }
+}
